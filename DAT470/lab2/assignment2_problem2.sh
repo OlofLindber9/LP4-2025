8fab599c10b7fe51bfe7d19132279995095d70c4
rikardro <93663695+rikardro@users.noreply.github.com>
2025-04-17 20:21:02 +0200

    dat470 lab2 problem 2a-f

--- DAT470/lab2/assignment2_problem2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 assignment2_problem2_skeleton.py  /data/courses/2025_dat470_dit066/gutenberg/huge
---
+++ DAT470/lab2/assignment2_problem2e.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT="assignment2_problem2e.py"
+
+echo "Running $SCRIPT with $1 worker(s)..."
+python3 "$SCRIPT"  -w "$1" /data/courses/2025_dat470_dit066/gutenberg/huge
