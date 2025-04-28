2cb35d9b20fa316e3a2136f04d8c48c13f7e4be1
rikardro <93663695+rikardro@users.noreply.github.com>
2025-04-28 16:22:30 +0200

    dat470 lab3 problem 1 (unfinished)

---
+++ DAT470/lab3/assignment3_problem1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from mrjob.job import MRJob
+
+class MRMineral(MRJob):
+    def mapper(self, _, line):
+        fields = line.split(',')
+        sun = f"{fields[1]} {fields[0]}"
+        mineral_value = int(fields[5])
+        yield (sun, mineral_value)
+
+    def reducer(self, sun, values):
+        total_value = sum(values)
+        yield (sun, total_value)
+
+
+if __name__ == '__main__':
+    MRMineral().run()
