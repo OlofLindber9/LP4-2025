ba089b055eb88df04eff16acce6fe690a879806f
rikardro <93663695+rikardro@users.noreply.github.com>
2025-05-13 09:59:59 +0200

    dat470 lab5 problem1

--- DAT470/lab5/assignment5_problem1.sh
+++ DAT470/lab5/assignment5_problem1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-FILEPATH = "/data/courses/2025_dat470_dit066/words"
-SCRIPT="assignment5_problem1.py"
+FILEPATH="/data/courses/2025_dat470_dit066/words"
+SCRIPT="assignment5_problem1_eval.py"
+SEED=0xee418b6c
 
-SEED = 0xee418b6c
+echo "Running $SCRIPT..."
+python3 "$SCRIPT"
