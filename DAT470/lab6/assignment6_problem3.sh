acf0fdba0e23c8ed5b4688a0de26fda1eaf48e65
rikardro <93663695+rikardro@users.noreply.github.com>
2025-05-22 17:02:54 +0200

    dat470 assignment6 problem4

--- DAT470/lab6/assignment6_problem3.sh
+++ DAT470/lab6/assignment6_problem3.sh
@@ -2,7 +2,7 @@
 
 FILE="/data/courses/2025_dat470_dit066/glove/glove.840B.300d.txt"
 QUERIES="/data/courses/2025_dat470_dit066/glove/queries.txt"
-SCRIPT="assignment6_problem2.py"
+SCRIPT="assignment6_problem3.py"
 
 echo "Running $SCRIPT on $FILE with $QUERIES..."
 python3 "$SCRIPT" "$FILE" "$QUERIES" -D $1
