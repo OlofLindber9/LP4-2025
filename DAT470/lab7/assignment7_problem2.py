968f8b5d861cb3c626d3d96f7b644f81fe65b891
rikardro <93663695+rikardro@users.noreply.github.com>
2025-05-27 20:23:31 +0200

    dat470 lab7 p2

---
+++ DAT470/lab7/assignment7_problem2.py
@@ -0,0 +1,155 @@
+import numpy as np
+import argparse
+import pandas as pd
+import csv
+import sys
+import time
+import cupy as cp
+import gc
+
+def linear_scan(X, Q, b):
+    """
+    Perform linear scan for querying nearest neighbor.
+    X: n*d dataset
+    Q: m*d queries
+    b: batch size
+    Returns an m-vector of indices I; the value i reports the row in X such 
+    that the Euclidean norm of ||X[I[i],:]-Q[i]|| is minimal
+    """
+    # Loop over the batches of queries
+    m = Q.shape[0]
+    I = cp.zeros(m, dtype=cp.int32)
+    for i in range(0, m, b):
+        end = min(i + b, m)
+        # Get the current batch of queries
+        Q_batch = Q[i:end]        
+        
+        # Compute the differences
+        D = Q_batch[:, cp.newaxis, :] - X[cp.newaxis, :, :]
+        
+        # Compute the Euclidean norm along the last axis of D
+        D_norm = cp.linalg.norm(D, axis=-1)
+
+        # Find the indices of the minimum distances
+        I[i:end] = cp.argmin(D_norm, axis=1)
+
+    return I
+
+def load_glove(fn):
+    """
+    Loads the glove dataset from the file
+    Returns (X,L) where X is the dataset vectors and L is the words associated
+    with the respective rows.
+    """
+    df = pd.read_table(fn, sep = ' ', index_col = 0, header = None,
+                           quoting = csv.QUOTE_NONE, keep_default_na = False)
+    X = np.ascontiguousarray(df, dtype = np.float32)
+    L = df.index.tolist()
+    return (X, L)
+
+def load_pubs(fn):
+    """
+    Loads the pubs dataset from the file
+    Returns (X,L) where X is the dataset vectors (easting,northing) and 
+    L is the list of names of pubs, associated with each row
+    """
+    df = pd.read_csv(fn)
+    L = df['name'].tolist()
+    X = np.ascontiguousarray(df[['easting','northing']], dtype = np.float32)
+    return (X, L)
+
+def load_queries(fn):
+    """
+    Loads the m*d array of query vectors from the file
+    """
+    return np.loadtxt(fn, delimiter = ' ', dtype = np.float32)
+
+def load_query_labels(fn):
+    """
+    Loads the m-long list of correct query labels from a file
+    """
+    with open(fn,'r') as f:
+        return f.read().splitlines()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( \
+          description = 'Perform nearest neighbor queries under the '
+          'Euclidean metric using linear scan, measure the time '
+          'and optionally verify the correctness of the results')
+    parser.add_argument(
+        '-d', '--dataset', type=str, required=True,
+        help = 'Dataset file (must be pubs or glove)')
+    parser.add_argument(
+        '-q', '--queries', type=str, required=True,
+        help = 'Queries file (must be compatible with the dataset)')
+    parser.add_argument(
+        '-l', '--labels', type=str, required=False,
+        help = 'Optional correct query labels; if provided, the correctness '
+        'of returned results is checked')
+    parser.add_argument(
+        '-b', '--batch-size', type=int, required=False, default=1000,
+        help = 'Size of batches')
+    args = parser.parse_args()
+
+    t1 = time.time()
+    if 'pubs' in args.dataset:
+        (X,L) = load_pubs(args.dataset)
+    elif 'glove' in args.dataset:
+        (X,L) = load_glove(args.dataset)
+    else:
+        sys.stderr.write(f'{sys.argv[0]}: error: Only glove/pubs supported\n')
+        exit(1)
+    t2 = time.time()
+
+    (n,d) = X.shape
+    assert len(L) == n
+
+    t3 = time.time()
+    Q = load_queries(args.queries)
+    t4 = time.time()
+
+    assert X.flags['C_CONTIGUOUS']
+    assert Q.flags['C_CONTIGUOUS']
+    assert X.dtype == np.float32
+    assert Q.dtype == np.float32
+    
+    m = Q.shape[0]
+    assert Q.shape[1] == d
+
+    t5 = time.time()
+    QL = None
+    if args.labels is not None:
+        QL = load_query_labels(args.labels)
+        assert len(QL) == m
+
+    # First access to the GPU is slower than later
+    _ = cp.asarray([], blocking = True)
+    t6 = time.time()
+    X_gpu = cp.asarray(X, blocking = True)
+    Q_gpu = cp.asarray(Q, blocking = True)
+    t7 = time.time()
+
+    I = linear_scan(X_gpu, Q_gpu, args.batch_size)
+    t8 = time.time()
+    
+    cp.cuda.Stream.null.synchronize()
+    
+    t9 = time.time()
+    I = cp.asnumpy(I, blocking = True)
+    t9 = time.time()
+    assert I.shape == (m,)
+
+    num_erroneous = 0
+    if QL is not None:
+        for (i,j) in enumerate(I):
+            if QL[i] != L[j]:
+                sys.stderr.write(f'{i}th query was erroneous: got "{L[j]}", '
+                                     f'but expected "{QL[i]}"\n')
+                num_erroneous += 1
+    print(f'Loading dataset ({n} vectors of length {d}) took', t2-t1)
+    print(f'Performing {m} NN queries took', t8-t7)
+    # report the times it takes to transfer the dataset and queries from host to device
+    print(f'Transferring dataset and queries from host to device took', t7-t6)
+    # report the time it takes to transfer the results from device to host
+    print(f'Transferring results from device to host took', t9-t8)
+    print(f'Number of erroneous queries: {num_erroneous}')
---
+++ DAT470/lab7/assignment7_problem2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --gres=gpu:L40s:1
+#SBATCH --mem-per-gpu=48G
+
+BATCH_SIZE=$1
+SIZE=$2
+
+DATA="/pubs/pubs.csv"
+DATASET="/data/courses/2025_dat470_dit066$DATA"
+QUERIES="/data/courses/2025_dat470_dit066/pubs/pub_queries_${SIZE}.txt"
+LABELS="/data/courses/2025_dat470_dit066/pubs/pub_queries_${SIZE}_names.txt"
+SCRIPT="assignment7_problem2.py"
+
+echo "Running $SCRIPT on $DATA with $SIZE queries, and batch size $BATCH_SIZE..."
+echo "Node: $SLURM_JOB_NODELIST"
+python3 "$SCRIPT" -d "$DATASET" -q "$QUERIES" -l "$LABELS" -b "$BATCH_SIZE"
